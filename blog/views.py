5bf289d21969379333b8f55b12b30f21f7aab177
firsenkodv <firsenkodv@rambler.rugit>
2019-03-09 20:30:43 +0600

    mail sending

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,36 @@ from . models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    return render (request, 'blog/post_list.html', {'posts': posts})
+    return render (request, 'blog/main.html', {'posts': posts})
+
+def main(request):
+    return render(request, 'blog/main.html',{})
+def news(request):
+    return render (request, "blog/news.html")
+
+from django.utils import timezone
+
+from django.core.mail import send_mail
+from .forms import SendingForm
+def send(request):
+    if (request.method == 'POST'):
+        form = SendingForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            send_mail(
+                '{} {}'.format(name, email),
+                'test meassage',
+                'firsenkodv@gmail.com',
+                ['firsenkodv@gmail.com', email],
+                fail_silently=False,
+            )
+            return render(request, 'blog/result.html', {
+                     'name': form.cleaned_data['name'],
+               'email': form.cleaned_data['email'],
+        })
+    else:
+        form = SendingForm()
+    return render(request, 'blog/send.html', {'form':form});
 
 # Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'firsenkodv@gmail.com'
+EMAIL_HOST_PASSWORD = "fd7771948fd"
